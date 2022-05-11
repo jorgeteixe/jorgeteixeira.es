de5f5483e2e24ecc0a588b323e4e98cf00e11c5b
Jorge Teixeira <jorge.teixeira@bizaway.com>
2022-05-11 15:32:12 +0200

    add coming soon banner

--- components/seo.js
+++ components/seo.js
@@ -3,9 +3,7 @@ import Head from 'next/head'
 function Seo({ title }) {
   return (
     <Head>
-      <title>
-        Jorge Teixeira {title ? ' | ' : ''} {title}
-      </title>
+      <title>Jorge Teixeira {title && ` | ${title}`}</title>
       <meta name="description" content="Jorge Teixeira" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
-import Header from '../components/header'
 import Seo from '../components/seo'
 
 export default function Home() {
   return (
     <>
       <Seo />
-      <Header />
+      <div className="h-screen flex justify-center">
+        <div className="flex flex-col justify-center text-center">
+          <p className="text-4xl">Jorge Teixeira</p>
+          <p className="text-lg font-light">Coming soon...</p>
+        </div>
+      </div>
     </>
   )
 }
