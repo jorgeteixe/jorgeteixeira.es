d384885108a6ad1e0ab61dea7e1aa4cb3c3f7456
Jorge Teixeira <jorge.teixeira@bizaway.com>
2022-05-11 21:09:06 +0200

    metatags open graph

--- components/seo.js
+++ components/seo.js
@@ -3,8 +3,38 @@ import Head from 'next/head'
 function Seo({ title }) {
   return (
     <Head>
-      <title>Jorge Teixeira {title && ` | ${title}`}</title>
-      <meta name="description" content="Jorge Teixeira" />
+      <title>
+        Jorge Teixeira |{' '}
+        {title || 'Creator. Mentor. Tech advocate based in Galicia.'}
+      </title>
+      <meta
+        name="description"
+        content="Using technology to build great things. Enjoy helping others do the same. Never stop learning. Never stop teaching."
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://jorgeteixeira.es/" />
+      <meta
+        property="og:title"
+        content="Jorge Teixeira — Creator. Mentor. Tech advocate based in Galicia."
+      />
+      <meta
+        property="og:description"
+        content="Using technology to build great things. Enjoy helping others do the same. Never stop learning. Never stop teaching."
+      />
+      <meta property="og:image" content="/open-graph-image.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://jorgeteixeira.es/" />
+      <meta
+        property="twitter:title"
+        content="Jorge Teixeira — Creator. Mentor. Tech advocate based in Galicia."
+      />
+      <meta
+        property="twitter:description"
+        content="Using technology to build great things. Enjoy helping others do the same. Never stop learning. Never stop teaching."
+      />
+      <meta property="twitter:image" content="/open-graph-image.png" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
