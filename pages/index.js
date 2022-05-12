dc999c486404360d838b6d79c11d673bfd121f09
Jorge Teixeira <jorge.teixeira@bizaway.com>
2022-05-12 17:49:30 +0200

    appear animation

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Seo from '../components/seo'
 import Navbar from '../components/navbar'
 import Skills from '../components/effects/skills'
 import BizAway from '../components/effects/bizaway'
+import Appear from '../components/effects/appear'
 
 export default function Home() {
   const skills = [
@@ -20,16 +21,23 @@ export default function Home() {
       <Seo />
       <div className="sm:h-screen flex flex-col text-gray-50 select-none">
         <div className="max-w-4xl p-9 sm:p-20">
-          <p className="text-5xl sm:text-6xl md:text-8xl mb-3 font-bold">
-            I&apos;m Jorge.
-          </p>
-          <p className="text-4xl sm:text-5xl font-semibold md:text-7xl text-gray-400 mb-4">
-            I use <Skills skills={skills} /> <br className="inline sm:hidden" />
-            to <br className="hidden sm:inline" /> build great things.
-          </p>
-          <p className="text-3xl sm:text-4xl font-semibold md:text-6xl text-gray-600">
-            Currently at <BizAway />.
-          </p>
+          <Appear>
+            <p className="text-5xl sm:text-6xl md:text-8xl mb-3 font-bold">
+              I&apos;m Jorge.
+            </p>
+          </Appear>
+          <Appear delay={0.1}>
+            <p className="text-4xl sm:text-5xl font-semibold md:text-7xl text-gray-400 mb-4">
+              I use <Skills skills={skills} />{' '}
+              <br className="inline sm:hidden" />
+              to <br className="hidden sm:inline" /> build great things.
+            </p>
+          </Appear>
+          <Appear delay={0.2}>
+            <p className="text-3xl sm:text-4xl font-semibold md:text-6xl text-gray-600">
+              Currently at <BizAway />.
+            </p>
+          </Appear>
         </div>
         <div className="mt-60 sm:grow sm:mt-0" />
         <div className="pb-8 max-w-4xl ">
